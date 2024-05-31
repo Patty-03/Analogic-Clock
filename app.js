ed0a6963904dc78387696430dedb3df19652bfb7
Patty <patriciamieres3003@gmail.com>
2024-05-30 20:11:53 -0400

    Terminando el reloj
    Adicion de un boton que cambia de reloj analogico a reloj digital. Implementacion del reloj digital

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function setClock(){
+function setAnalogicClock(){
     let ticTacSound = new Audio('./Media/tic-tac.mp3');
     let now = new Date();
     let hours = now.getHours();
@@ -13,7 +13,48 @@ function setClock(){
     document.querySelector('.min-hand').style.transform = "rotate(" + minValue + "deg)";
     document.querySelector('.sec-hand').style.transform = "rotate(" + secValue + "deg)";
     ticTacSound.play();
+}
+
+function setDigitalClock(){
+    let now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+
+    document.querySelector('.hours').textContent = hours;
+    document.querySelector('.minutes').textContent = minutes;
+    if(seconds < 10){
+        document.querySelector('.seconds').textContent = '0' + seconds;
+    }
+    else
+    document.querySelector('.seconds').textContent = seconds;
+
+
+    let amPm = document.querySelector('.meridian');
+
+    if(hours < 12){
+        amPm.textContent = 'AM';
+    }
+}
+
+const toggleBtn = document.querySelector('.btn');
+
+function toggleClock(){
+    let analogic = document.querySelector('.analog-clock');
+    let digital =  document.querySelector('.digital-clock');
+
+    analogic.classList.toggle('invisible');
+    digital.classList.toggle('invisible');
+
+    if(toggleBtn.textContent === 'Digital Clock'){
+        toggleBtn.textContent = 'Analogic Clock';
+    }
+    else{
+        toggleBtn.textContent = 'Digital Clock';
+    }
 
 }
 
-setInterval(setClock, 1000);
+setInterval(setAnalogicClock, 1000);
+setInterval(setDigitalClock, 1000);
+toggleBtn.addEventListener('click', toggleClock);
